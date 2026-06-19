3f3faa115491324bb321ec2e3bd6424195c15f8f
Devansh Gupta <157011724+dg2201@users.noreply.github.com>
2026-06-19 11:06:38 +0530

    Time: 4 ms (38.86%) | Memory: 44 MB (5.09%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,25 +1,33 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if (ch == ')' && top != '(') {
-                    return false;
-                }
-                if (ch == ']' && top != '[') {
-                    return false;
-                }
-                if (ch == '}' && top != '{') {
-                    return false;
-                }
+        Stack<Character> letterStack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')' || s.charAt(i) == ']' || s.charAt(i) == '}') {
+              if(letterStack.isEmpty()){
+                  return false;
+                  //if the upcoming character is a closing bracket and the stack is empty then 
+                  //instantly return false obv
+              }
+              else if(letterStack.peek() == '(' && s.charAt(i) == ')'){
+                  letterStack.pop();
+              }
+              else if(letterStack.peek() == '[' && s.charAt(i) == ']'){
+                  letterStack.pop();
+              }
+              else if(letterStack.peek() == '{' && s.charAt(i) == '}'){
+                  letterStack.pop();
+              }
+              else {
+                letterStack.push(s.charAt(i));
+              }
+            }
+            else{
+                letterStack.push(s.charAt(i));
             }
         }
-        return stack.isEmpty();
+        return letterStack.isEmpty();
     }
+        
 }
